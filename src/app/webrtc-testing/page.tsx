6462d3c6e0a848dca44fdd894ac3044b5cf44ba8
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2025-12-10 02:43:48 +0530

    fix

---
+++ src/app/webrtc-testing/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+// import "@/lib/PeerToPeer";
+
+const WebRtcTesting = () => {
+    useEffect(() => {
+        console.log("WebRTC Mounted");
+    }, []);
+    return <div></div>;
+};
+
+export default WebRtcTesting;
