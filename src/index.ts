b35fd31258fcdec14574fe37f8d661ddab51bbda
BABAR-TAHSEEN55 <babarsmoke24@gmail.com>
2026-01-31 22:08:56 +0530

    refactor: Added eslintignore check

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,15 @@ if (!databaseURL) {
 }
 
 export const db = drizzle(databaseURL);
+
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL!,
+//   ssl: {
+//     rejectUnauthorized: true,
+//   },
+// });
+
+// export const db = drizzle(pool);
